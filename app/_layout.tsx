3d0821db4f976f70c57d407f7bc7e55e9d60769f
GuillermoRivero98 <141684687+GuillermoRivero98@users.noreply.github.com>
2024-11-04 19:13:38 -0300

    .

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -11,20 +11,7 @@ import { useColorScheme } from '@/hooks/useColorScheme';
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-  });
 
-  useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
-    }
-  }, [loaded]);
-
-  if (!loaded) {
-    return null;
-  }
 
   return (
       <Stack>
